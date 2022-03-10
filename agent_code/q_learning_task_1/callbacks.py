d9b05f37e1e8d808b4be79b4accdefe0331c1d2c
buermarc <buermarc2@gmail.com>
2022-03-10 23:57:26 +0100

    Revert "Change how we decide what to do"
    This reverts commit 0c20b32f91d39d04d34a324a94e946707586f570.

--- agent_code/q_learning_task_1/callbacks.py
+++ agent_code/q_learning_task_1/callbacks.py
@@ -16,14 +16,24 @@ def act(self, game_state: dict):
 
     game_state = convert_to_state_object(game_state)
     feature_vector = extract_features(game_state)
-    if self.train and np.random.rand() > 0.8 and False:
-        selected_action = np.random.choice(ACTIONS)
+    if self.train:
+
+        probabilities = self.q_table[feature_vector.to_state()]
+        self.logger.debug(f'Current train probabilities: {probabilities}')
+
+        probabilities -= np.min(probabilities)
+        prob_sum = np.sum(probabilities)
+
+        if prob_sum != 0:
+            probabilities = probabilities / prob_sum
+        else:
+            probabilities = None
+
+        selected_action = np.random.choice(ACTIONS, p=probabilities)
         self.logger.info(f"Selected action: {selected_action}")
         return selected_action
     else:
         # TODO Select action from self.q_table
         feature_state = feature_vector.to_state()
-        props = self.q_table[feature_state]
-        #print(f"'UP',{props[0]:0.2f} 'RIGHT',{props[1]:0.2f} 'DOWN',{props[2]:0.2f} 'LEFT',{props[3]:0.2f} 'WAIT' {props[4]:0.2f}")
-        action_indicies = np.argwhere(self.q_table[feature_state] == np.max(self.q_table[feature_state])).T[0]
-        return ACTIONS[np.random.choice(action_indicies)]
+        action_index = np.argmax(self.q_table[feature_state])
+        return ACTIONS[action_index]
