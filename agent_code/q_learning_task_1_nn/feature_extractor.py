d9c95ea2cddc3a427d451b7fc757fe3ece19e885
d070494 <simon.kurt.petrik@sap.com>
2022-03-16 21:53:24 +0100

    Deep Q Learning

---
+++ agent_code/q_learning_task_1_nn/feature_extractor.py
@@ -0,0 +1,236 @@
+from __future__ import annotations
+
+from typing import Dict, List, Tuple
+
+import numpy as np
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
+from agent_code.q_learning_task_1_nn.direction import Direction
+from agent_code.q_learning_task_1_nn.feature_vector import FeatureVector, Neighborhood
+from agent_code.q_learning_task_1_nn.game_state import GameState
+from agent_code.q_learning_task_1_nn.player import Player
+from agent_code.q_learning_task_1_nn.types import Position, Bomb
+
+
+def convert_to_state_object(state: Dict) -> GameState:
+    rnd: int = state['round']
+    step: int = state['step']
+    field: np.ndarray = state['field']
+    bombs: List[Bomb] = state['bombs']
+    explosion_map: np.ndarray = state['explosion_map']
+    coins: List[Position] = state['coins']
+    self: Player = Player(state['self'])
+    others: List[Player] = list(map(Player, state['others']))
+    user_input: str | None = state['user_input']
+    return GameState(rnd, step, field.T, bombs, explosion_map, coins, self, others, user_input)
+
+
+def calculate_neighborhood_distance(
+    field: np.ndarray,
+    origin: Position,
+    destinations: List[Position],
+    bombs: List[Bomb],
+    with_crates: bool = True,
+    with_bombs: bool = True,
+) -> Neighborhood:
+
+    field: np.ndarray = field.copy()
+
+    # Make creates to obstacles for pathfinding
+    field[field > 0] = -1
+    # Make free fields to pathfinding free fields
+    field[field == 0] = 1
+
+    if with_bombs:
+        for bomb_coord, time in bombs:
+            field[bomb_coord] = 1
+
+    neighborhood = Neighborhood()
+    grid = Grid(matrix=field)
+    finder = AStarFinder()
+
+    for d in Direction:
+        name, coords = d.value
+        shortest_path = float('inf')
+
+        for dest in destinations:
+            x = origin[0] + coords[0]
+            y = origin[1] + coords[1]
+            if field[x][y] > 0:
+                start = grid.node(x, y)
+                end = grid.node(dest[0], dest[1])
+
+                path, runs = finder.find_path(start, end, grid)
+                grid.cleanup()
+
+                if len(path) < shortest_path:
+                    shortest_path = len(path)
+
+            else:
+                break
+
+        setattr(neighborhood, name, shortest_path)
+
+    return neighborhood
+
+
+def calculate_neighborhood_distance_for_bombs(
+    field: np.ndarray,
+    origin: Position,
+    destinations: List[Position],
+    bombs: List[Bomb],
+) -> Neighborhood:
+
+    field: np.ndarray = field.copy()
+
+    # Make creates to obstacles for pathfinding
+    field[field > 0] = -1
+    # Make free fields to pathfinding free fields
+    field[field == 0] = 1
+
+    for bomb_coord, time in bombs:
+        field[bomb_coord] = 1
+
+    neighborhood = Neighborhood()
+    grid = Grid(matrix=field)
+    finder = AStarFinder()
+
+    for d in Direction:
+        name, coords = d.value
+        longest_path = 0
+
+        for bomb in destinations:
+            if (origin[0], origin[1]) == (bomb[0], bomb[1]):
+                return Neighborhood(1, 1, 1, 1)
+            x = origin[0] + coords[0]
+            y = origin[1] + coords[1]
+            if field[x][y] > 0:
+                start = grid.node(x, y)
+                end = grid.node(bomb[0], bomb[1])
+
+                path, runs = finder.find_path(start, end, grid)
+                grid.cleanup()
+
+                # if len(path) > longest_path:
+                #     longest_path = len(path)
+                longest_path += len(path)
+
+            else:
+                continue
+
+        setattr(neighborhood, name, longest_path)
+
+    return neighborhood
+
+
+def can_move(field: np.ndarray, position: Position) -> Neighborhood:
+    neighborhood = Neighborhood()
+    for d in Direction:
+        name, coords = d.value
+        x = position[0] + coords[0]
+        y = position[1] + coords[1]
+        move_possible = field[x, y] == 0
+        setattr(neighborhood, name, move_possible)
+    return neighborhood
+
+
+def try_to_move_into_safety(origin: Position, bombs: List[Bomb], bomb_distance: Neighborhood, can_move_in_direction: Neighborhood) -> Neighborhood:
+    """
+    Go into each direction and check if it safe.
+
+    We check for each direction if we would still be in danger.
+    To see if something is really safe we also check if we can move towards it.
+    Still existing valid paths are keept, invalid paths (not movable, and not
+    safe) are set to -1.
+    """
+
+    safety = []
+    for d in Direction:
+        _, coords = d.value
+        new_origin = (origin[0] + coords[0], origin[1] + coords[1])
+        safety.append(not is_in_danger(new_origin, bombs))
+
+    for i, move in enumerate(can_move_in_direction):
+        safety[i] = move and safety[i]
+
+    if True in safety:
+        for (i, d) in enumerate(Direction):
+            if not safety[i]:
+                setattr(bomb_distance, d.value[0], 0)
+
+    return bomb_distance
+
+
+def is_in_danger(origin: Position, bombs: List[Bomb]) -> bool:
+    in_danger = False
+
+    for bomb_coords, exp_time in bombs:
+        for i in range(2):
+            if origin[i] == bomb_coords[i]:
+                other_i = (i + 1) % 2
+                dist = abs(origin[other_i] - bomb_coords[other_i])
+                if dist < 4:
+                    in_danger = True
+                    break
+
+    return in_danger
+
+
+def move_to_danger(field: np.ndarray, origin: Position, bombs: List[Bomb], explosion_map: np.ndarray):
+    neighborhood = Neighborhood()
+    for d in Direction:
+        name, coords = d.value
+        x = origin[0] + coords[0]
+        y = origin[1] + coords[1]
+
+        in_danger = False
+
+        if field[x, y] == 0:
+            for bomb_coords, _ in bombs:
+                if x == bomb_coords[0] or y == bomb_coords[1]:
+                    dist = max(abs(x - bomb_coords[0]), abs(y - bomb_coords[1]))
+
+                    if dist <= 4:
+                        in_danger = True
+                        break
+
+            if not in_danger:
+                if explosion_map[x, y] > 0:
+                    in_danger = True
+
+        setattr(neighborhood, name, in_danger)
+
+    return neighborhood
+
+
+def extract_crates(field: np.ndarray) -> List[Position]:
+    crates = np.where(field == 1)
+    return list(np.array(crates).T)
+
+
+def extract_features(state: GameState) -> FeatureVector:
+
+    coin_exists = len(state.coins) > 0
+    coin_distance = calculate_neighborhood_distance(state.field, state.self.position, state.coins, state.bombs)
+
+    crates = extract_crates(state.field)
+
+    crate_exists = len(crates) > 0
+    crate_distance = calculate_neighborhood_distance(state.field, state.self.position, crates, state.bombs)
+
+    can_move_in_direction = can_move(state.field, state.self.position)
+
+    bombs = [(x, y) for ((x, y), _) in state.bombs]
+    bomb_exists = len(bombs) > 0
+    bomb_distance = calculate_neighborhood_distance_for_bombs(state.field, state.self.position, bombs, state.bombs)
+    bomb_distance = try_to_move_into_safety(state.self.position, state.bombs, bomb_distance, can_move_in_direction)
+
+    in_danger = is_in_danger(state.self.position, state.bombs)
+
+    if not in_danger:
+        mv_to_danger = move_to_danger(state.field, state.self.position, state.bombs, state.explosion_map)
+    else:
+        mv_to_danger = Neighborhood(False, False, False, False)
+
+    return FeatureVector(coin_distance, coin_exists, crate_distance, crate_exists, in_danger, can_move_in_direction, bomb_distance, bomb_exists, mv_to_danger)
---
+++ agent_code/q_learning_task_2_nn/train.py
@@ -0,0 +1,166 @@
+from __future__ import annotations
+
+from os.path import join, dirname, isfile
+from typing import List, Optional
+
+import numpy as np
+import torch
+
+from agent_code.q_learning_task_1_nn import rewards
+from agent_code.q_learning_task_1_nn.feature_extractor import extract_features, convert_to_state_object
+from agent_code.q_learning_task_1_nn.feature_vector import FeatureVector
+from agent_code.q_learning_task_1_nn.q_nn import QNN
+from torch import optim
+import torch.nn as nn
+
+import os
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+Q_NN_FILE = os.environ.get("Q_NN_FILE", join(dirname(__file__), 'qnn.pt'))
+STATS_FILE = os.environ.get("STATS_FILE", join(dirname(__file__), 'q_learning_task_2_nn.txt'))
+
+# Hyperparameter
+gamma = 1
+alpha = 0.05
+
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+
+    if isfile(Q_NN_FILE):
+        self.model = QNN(FeatureVector.size(), len(ACTIONS))
+        self.model.load_state_dict(torch.load(Q_NN_FILE))
+        self.model.eval()
+    else:
+        self.model = QNN(FeatureVector.size(), len(ACTIONS))
+
+    self.optimizer = optim.Adam(self.model.parameters())
+    self.criterion = nn.MSELoss()
+
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    if old_game_state:
+        old_state = convert_to_state_object(old_game_state)
+        current_feature_state = extract_features(old_state)
+        new_state = convert_to_state_object(new_game_state)
+        next_feature_state = extract_features(new_state)
+
+        custom_events = extract_events_from_state(self, current_feature_state, next_feature_state)
+
+        total_events = custom_events + events
+
+        update_nn(self, current_feature_state, next_feature_state, self_action, total_events)
+
+
+def update_nn(self, current_feature_state: FeatureVector, next_feature_state: Optional[FeatureVector],
+              self_action: str, total_events: List[str]):
+    reward = reward_from_events(self, total_events)
+
+    current_action_index = ACTIONS.index(self_action)
+
+    prediction = self.model(current_feature_state.to_nn_state())
+    target = prediction.clone()
+
+    if next_feature_state:
+        q_next = np.max(prediction.detach().numpy())
+    else:
+        q_next = 0
+
+    q_updated = reward + gamma * q_next
+
+    target[current_action_index] = q_updated
+
+    loss = self.criterion(prediction, target)
+    loss.backward()
+    self.optimizer.step()
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+    This replaces game_events_occurred in this round.
+
+    This is similar to game_events_occurred. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+    old_state = convert_to_state_object(last_game_state)
+    current_feature_state = extract_features(old_state)
+
+    update_nn(self, current_feature_state, None, last_action, events)
+
+    with open(STATS_FILE, 'a+') as f:
+        f.write(f'{len(old_state.coins)}, ')
+    torch.save(self.model.state_dict(), Q_NN_FILE)
+
+
+def extract_events_from_state(self, old_features: FeatureVector, new_features: FeatureVector) -> List:
+    custom_events = []
+    if old_features.coin_distance.minimum() < new_features.coin_distance.minimum():
+        custom_events.append(rewards.MOVED_AWAY_FROM_COIN)
+    elif old_features.coin_distance.minimum() > new_features.coin_distance.minimum():
+        custom_events.append(rewards.APPROACH_COIN)
+
+    if old_features.coin_distance.minimum() < new_features.coin_distance.minimum():
+        custom_events.append(rewards.MOVED_AWAY_FROM_COIN)
+    elif old_features.coin_distance.minimum() > new_features.coin_distance.minimum():
+        custom_events.append(rewards.APPROACH_COIN)
+
+    if old_features.crate_distance.minimum() < new_features.crate_distance.minimum():
+        custom_events.append(rewards.MOVED_AWAY_FROM_CRATE)
+    elif old_features.crate_distance.minimum() > new_features.crate_distance.minimum():
+        custom_events.append(rewards.APPROACH_CRATE)
+
+    if old_features.bomb_exists and old_features.bomb_distance.maximum() < new_features.bomb_distance.maximum():
+        custom_events.append(rewards.MOVED_AWAY_FROM_BOMB)
+    elif old_features.bomb_exists and old_features.bomb_distance.maximum() == new_features.bomb_distance.maximum() and new_features.in_danger:
+        custom_events.append(rewards.APPROACH_BOMB)
+    elif old_features.bomb_exists and old_features.bomb_distance.maximum() > new_features.bomb_distance.maximum():
+        custom_events.append(rewards.APPROACH_BOMB)
+
+    if new_features.in_danger:
+        custom_events.append(rewards.IN_DANGER)
+
+    return custom_events
+
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Here you can modify the rewards your agent get so as to en/discourage
+    certain behavior.
+    """
+    reward_sum = 0
+    for event in events:
+        if event in rewards.rewards:
+            reward_sum += rewards.rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
