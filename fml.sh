34121d46c49c378287283b4aed1d3c7d94f81cfe
buermarc <buermarc2@gmail.com>
2022-03-24 22:25:58 +0100

    fml.sh optional variable COUNT in $4

--- fml.sh
+++ fml.sh
@@ -2,7 +2,7 @@
 REPO_DIR=$(pwd)
 
 if [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ -z "$1" ] ;then
-    echo sh fml.sh [agents] [scenario] [rounds]
+    echo sh fml.sh AGENTS SCENARIO ROUNDS [count]
     exit
 fi
 
@@ -24,6 +24,12 @@ else
     ROUNDS=100
 fi
 
+if [ ! -z "$4" ];then
+    COUNT=$4
+else
+    COUNT=20
+fi
+
 TMP_DIR=$(mktemp -d)
 mkdir -p ${TMP_DIR}/agents_blobs
 
@@ -39,12 +45,13 @@ if [ ! -f "main.py" ];then
     exit
 fi
 
-for i in {0..20};do
+for i in $( seq 0  ${COUNT} );do
     tmux neww -t train
     tmux send "pushd ${REPO_DIR}" ENTER
     tmux send "source venv/bin/activate" ENTER
     tmux send "export Q_TABLE_FILE=${TMP_DIR}/agents_blobs/${AGENTS}_${i}.npy" ENTER
     tmux send "export STATS_FILE=${TMP_DIR}/agents_blobs/stats_${AGENTS}_${i}.txt" ENTER
     tmux send "python main.py play --scenario ${SCENARIO} --agents ${AGENTS} --n-rounds ${ROUNDS} --train 1 --no-gui" ENTER
+    tmux send "exit" ENTER
     echo $i
 done
