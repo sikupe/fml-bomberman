961ea370ce26b2fef16da1b1cf3f60165a0c3441
buermarc <buermarc2@gmail.com>
2022-03-24 19:00:01 +0100

    Add transitions to agents, dismiss unknown events

--- agent_code/q_learning_task_3/train.py
+++ agent_code/q_learning_task_3/train.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from collections import deque, namedtuple
 from os.path import join, dirname, isfile
 from typing import List
 
@@ -20,6 +21,11 @@ ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 Q_TABLE_FILE = os.environ.get("Q_TABLE_FILE", join(dirname(__file__), 'q_learning_task_3.npy'))
 STATS_FILE = os.environ.get("STATS_FILE", join(dirname(__file__), 'stats_q_learning_task_2.txt'))
 
+TRANSITION_HISTORY_SIZE = 10
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
 # Hyperparameter
 gamma = 0.9
 alpha = 0.05
@@ -36,6 +42,7 @@ def setup_training(self):
 
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
 
     if isfile(Q_TABLE_FILE):
         self.q_table = np.load(Q_TABLE_FILE)
@@ -60,10 +67,12 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     :param new_game_state: The state the agent is in now.
     :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
     """
+    new_state = convert_to_state_object(new_game_state)
+    self.transitions.append(new_state)
+
     if old_game_state:
         old_state = convert_to_state_object(old_game_state)
         current_feature_state = extract_features(old_state, FeatureVector)
-        new_state = convert_to_state_object(new_game_state)
         next_feature_state = extract_features(new_state, FeatureVector)
 
         custom_events = extract_events_from_state(self, current_feature_state, next_feature_state, self_action)
@@ -121,6 +130,6 @@ def reward_from_events(self, events: List[str]) -> int:
         if event in rewards.rewards:
             reward_sum += rewards.rewards[event]
         else:
-            raise Exception(f"Event is not in reward list: {event}")
+            self.logger.error("Event is not in reward list: %s", event)
     self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
     return reward_sum
