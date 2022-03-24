2549c04443b06ab341578f232f6b5836a3334444
buermarc <buermarc2@gmail.com>
2022-03-24 18:18:18 +0100

    Fix import FeatureVector and feature_extractor in q_learning_task_3_extended_feature_space

--- agent_code/q_learning_task_3_extended_feature_space/callbacks.py
+++ agent_code/q_learning_task_3_extended_feature_space/callbacks.py
@@ -4,8 +4,9 @@ import numpy as np
 
 from agent_code.common.callbacks import q_table_act
 from agent_code.common.feature_extractor import convert_to_state_object
-from agent_code.q_learning_task_3_extended_feature_space.feature_extractor import extract_features
+from agent_code.common.feature_extractor import extract_features
 from agent_code.q_learning_task_3_extended_feature_space.train import ACTIONS, Q_TABLE_FILE
+from agent_code.q_learning_task_3_extended_feature_space.feature_vector import FeatureVector
 
 epsilon = 0.1
 
@@ -17,6 +18,6 @@ def setup(self):
 
 def act(self, game_state: dict):
     game_state = convert_to_state_object(game_state)
-    feature_vector = extract_features(game_state)
+    feature_vector = extract_features(game_state, FeatureVector)
 
     return q_table_act(self, feature_vector, ACTIONS, epsilon)
