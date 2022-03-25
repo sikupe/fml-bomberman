240459d6e858ff692c9dfc97e05b13a478537f7c
buermarc <buermarc2@gmail.com>
2022-03-25 18:58:26 +0100

    merge.py: support nn models

--- tools/merge.py
+++ tools/merge.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import sys
 import logging
@@ -8,12 +9,10 @@ parser = argparse.ArgumentParser(description="Merge multiple trained models")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-parser.add_argument(
-    "path", type=str, help="Directory containing the trained models"
-)
+parser.add_argument("path", type=str, help="Directory containing the trained models")
 
 parser.add_argument(
-    "destination", type=str, help="Optional path of the output file", nargs='?'
+    "destination", type=str, help="Optional path of the output file", nargs="?"
 )
 
 
@@ -29,10 +28,17 @@ if __name__ == "__main__":
     if args.destination:
         destination = args.destination
     else:
-        destination = os.path.join(os.getcwd(), "merged_model.npy")
+        destination = os.path.join(os.getcwd(), "merged_model")
 
-    model_files = [file for file in os.listdir(abs_path) if ".npy" in file]
-    models = [np.load(os.path.join(abs_path, file)) for file in model_files]
-    merged_model = np.mean(np.array(models), axis=0)
-    np.save(destination, merged_model)
-    logger.info("Saved model to %s", destination)
+    for file_extension in [".npy", ".pt"]:
+        model_files = [file for file in os.listdir(abs_path) if file_extension in file]
+        if len(model_files) == 0:
+            continue
+        models = [np.load(os.path.join(abs_path, file)) for file in model_files]
+        merged_model = np.mean(np.array(models), axis=0)
+        # If user supplied destination, don't add file_extension
+        model_file = destination if args.destination else destination + file_extension
+        np.save(model_file, merged_model)
+        print(
+            f"{file_extension}: Saved model to {model_file}. Model shape {merged_model.shape}."
+        )
