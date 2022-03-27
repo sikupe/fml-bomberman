64f475cf803f47239d7ff0797303ab1241064431
buermarc <buermarc2@gmail.com>
2022-03-27 11:41:10 +0200

    Set rewards through through env var

--- agent_code/q_learning_task_3_advanced_features/rewards.py
+++ agent_code/q_learning_task_3_advanced_features/rewards.py
@@ -1,38 +1,45 @@
 import events as e
+import json
+import os
 
 from agent_code.common.events import MOVE_IN_DANGER, MOVE_OUT_OF_DANGER, GOOD_BOMB, BAD_BOMB, IN_DANGER, \
     APPROACH_SECURITY, MOVED_AWAY_FROM_SECURITY, APPROACH_COIN, MOVED_AWAY_FROM_COIN, APPROACH_CRATE, \
     MOVED_AWAY_FROM_CRATE, APPROACH_OPPONENT, MOVED_AWAY_FROM_OPPONENT, WIGGLE
 
-rewards = {
-    IN_DANGER: -20,
-    MOVE_IN_DANGER: -35,
-    MOVE_OUT_OF_DANGER: 35,
-    GOOD_BOMB: 45,
-    BAD_BOMB: -40,
-    APPROACH_SECURITY: 15,
-    MOVED_AWAY_FROM_SECURITY: -15,
-    APPROACH_COIN: 9,
-    MOVED_AWAY_FROM_COIN: -9,
-    APPROACH_CRATE: 9,
-    MOVED_AWAY_FROM_CRATE: -9,
-    APPROACH_OPPONENT: 7,
-    MOVED_AWAY_FROM_OPPONENT: -7,
-    WIGGLE: -2,
-    e.COIN_COLLECTED: 25,
-    e.MOVED_UP: -3,
-    e.MOVED_DOWN: -3,
-    e.MOVED_LEFT: -3,
-    e.MOVED_RIGHT: -3,
-    e.INVALID_ACTION: -60,
-    e.WAITED: -15,
-    e.SURVIVED_ROUND: 5,
-    e.BOMB_DROPPED: 10,
-    e.BOMB_EXPLODED: 0,
-    e.COIN_FOUND: 30,
-    e.CRATE_DESTROYED: 40,
-    e.GOT_KILLED: -200,
-    e.KILLED_SELF: -200,
-    e.KILLED_OPPONENT: 50,
-    e.OPPONENT_ELIMINATED: 0,
-}
+REWARDS = os.environ.get("REWARDS", "UNSET")
+
+if REWARDS != "UNSET":
+    rewards = json.loads(REWARDS)
+else:
+    rewards = {
+        IN_DANGER: -20,
+        MOVE_IN_DANGER: -35,
+        MOVE_OUT_OF_DANGER: 35,
+        GOOD_BOMB: 45,
+        BAD_BOMB: -40,
+        APPROACH_SECURITY: 15,
+        MOVED_AWAY_FROM_SECURITY: -15,
+        APPROACH_COIN: 9,
+        MOVED_AWAY_FROM_COIN: -9,
+        APPROACH_CRATE: 9,
+        MOVED_AWAY_FROM_CRATE: -9,
+        APPROACH_OPPONENT: 7,
+        MOVED_AWAY_FROM_OPPONENT: -7,
+        WIGGLE: -2,
+        e.COIN_COLLECTED: 25,
+        e.MOVED_UP: -3,
+        e.MOVED_DOWN: -3,
+        e.MOVED_LEFT: -3,
+        e.MOVED_RIGHT: -3,
+        e.INVALID_ACTION: -60,
+        e.WAITED: -15,
+        e.SURVIVED_ROUND: 5,
+        e.BOMB_DROPPED: 10,
+        e.BOMB_EXPLODED: 0,
+        e.COIN_FOUND: 30,
+        e.CRATE_DESTROYED: 40,
+        e.GOT_KILLED: -200,
+        e.KILLED_SELF: -200,
+        e.KILLED_OPPONENT: 50,
+        e.OPPONENT_ELIMINATED: 0,
+    }
