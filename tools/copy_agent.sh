d18422b6e86943838bee492fe074d2b9e4f54cd7
buermarc <buermarc2@gmail.com>
2022-03-27 13:40:27 +0200

    copy_agent script: see result of run

--- tools/copy_agent.sh
+++ tools/copy_agent.sh
@@ -6,8 +6,29 @@ AGENT_DIR=/home/mulc/repos/bomberman/save-bomb/agent_code/q_learning_task_3_adva
 AGENT_NAME=q_learning_task_3_advanced_features
 
 pushd /tmp
+# If the repo exists reclone it
+if [ -d ./final_bomberman ];then
+    rm -rf ./final_bomberman
+fi
 git clone https://github.com/ukoethe/bomberman_rl final_bomberman
 pushd final_bomberman
+
+# Apply patch to see result with --no-gui
+git apply <<EOF
+diff --git a/environment.py b/environment.py
+index 0ec3726..6d094a7 100644
+--- a/environment.py
++++ b/environment.py
+@@ -507,6 +507,7 @@ class BombeRLeWorld(GenericWorld):
+         for a in self.agents:
+             # Send exit message to shut down agent
+             self.logger.debug(f'Sending exit message to agent <{a.name}>')
++            print(f"{a.name}: {a.total_score}")
+             # todo multiprocessing shutdown
+ 
+ 
+EOF
+
 pushd agent_code
 
 cp -r $AGENT_DIR strong_students
@@ -26,4 +47,4 @@ popd
 
 podman build -t test_strong_students .
 
-podman run -i test_strong_students bash -c 'python3 main.py play --agents strong_students'
+podman run -i test_strong_students bash -c 'python3 main.py play --agents strong_students --no-gui'
